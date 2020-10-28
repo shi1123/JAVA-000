472078b453e6b98f7c30bec9219d17d832d65478
shi1123 <1009608989@qq.com>
2020-10-28 23:52:12 +0800

    week02 homework

---
+++ Week_02/JvmLearn/src/main/java/com/szp/geektime/javaClass/week02/MyHttpServer.java
@@ -0,0 +1,20 @@
+package com.szp.geektime.javaClass.week02;
+
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
+
+public class MyHttpServer {
+    public static void main(String[] args) throws IOException {
+        OkHttpClient client = new OkHttpClient();
+        try {
+            Request request = new Request.Builder().url("http://localhost:8801/test").build();
+            Response response = client.newCall(request).execute();
+            System.out.println(response.body().string());
+        } finally {
+            client.clone();
+        }
+    }
+}
